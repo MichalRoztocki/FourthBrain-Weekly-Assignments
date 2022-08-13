8a897167a684a020487564a9cebe2505a3b3258a
michal <mroztocki1@gmail.com>
2022-08-13 05:52:14 -0400

    Add sections to streamlit

--- week-7-supervised-ml-deep-learning/nb/fuel-efficiency-prediction-streamlit.py
+++ week-7-supervised-ml-deep-learning/nb/fuel-efficiency-prediction-streamlit.py
@@ -4,16 +4,27 @@ import plotly.figure_factory as ff
 import streamlit as st
 import streamlit.components.v1 as components
 from PIL import Image
+import joblib
+import shap
 
 # Add and resize an image to the top of the app
-img_fuel = Image.open("../img/fuel_efficiency.png")
+img_fuel = Image.open("img/fuel_efficiency.png")
 st.image(img_fuel, width=700)
 
 st.markdown("<h1 style='text-align: center; color: black;'>Fuel Efficiency</h1>", unsafe_allow_html=True)
 
 # Import train dataset to DataFrame
-train_df = pd.read_csv("../dat/train.csv.gz", compression="gzip")
-model_results_df = pd.read_csv("../dat/model_results.csv")
+train_df = pd.read_csv("dat/X_train.csv", index_col=0)
+model_results_df = pd.read_csv("dat/Results.csv", index_col=0)
+test_df = pd.read_csv("dat/X_test.csv", index_col=0)
+tree_test_df = pd.read_csv("dat/X_tree_test.csv", index_col=0)
+
+# Import shap datasets to DataFrame
+dnn_shap_local = pd.read_csv("dat/dnn_shap_local.csv")
+dnn_shap_global = pd.read_csv("dat/dnn_shap_global.csv")
+
+tpot_shap_local = pd.read_csv("dat/tpot_shap_local.csv")
+tpot_shap_global = pd.read_csv("dat/tpot_shap_global.csv")
 
 # Create sidebar for user selection
 with st.sidebar:
@@ -23,7 +34,7 @@ with st.sidebar:
     # Available models for selection
 
     # YOUR CODE GOES HERE!
-    models = ["DNN", "TPOT"]
+    models = ["Linear", "DNN", "TPOT"]
 
     # Add model select boxes
     model1_select = st.selectbox(
@@ -64,27 +75,48 @@ with tab2:
     # Columns for side-by-side model comparison
     col1, col2 = st.columns(2)
 
+    model_dict = {"DNN": "dnn_model", "TPOT": "tpot_extratrees", "Linear": "linear_model"}
     # Build the confusion matrix for the first model.
     with col1:
         st.header(model1_select)
 
         # YOUR CODE GOES HERE!
-
+        row_name = model_dict[model1_select]
+        st.write(model_results_df.loc[row_name])
 
     # Build confusion matrix for second model
     with col2:
         st.header(model2_select)
 
         # YOUR CODE GOES HERE!
+        row_name = model_dict[model2_select]
+        st.write(model_results_df.loc[row_name])
 
 
 with tab3: 
     # YOUR CODE GOES HERE!
         # Use columns to separate visualizations for models
         # Include plots for local and global explanability!
-     
-    st.header(model1_select)
-    
-    st.header(model2_select)
+    # st.header(model1_select)
+    st.header("DNN")
 
+    # shap_dict = {"DNN": "dnn_explainer", "TPOT": "tpot_explainer"}
+    # filename_1 = "dat/" + shap_dict[model1_select] + ".bz2"
+    filename_1 = "dat/dnn_explainer.bz2"
+
+    explainer_1 = joblib.load(filename=filename_1)
+    st.write(shap.initjs())
+    st.write(shap.force_plot(explainer_1.expected_value, dnn_shap_local[0], \
+                test_df.iloc[5,:]))
+
+    # st.header(model2_select)
+    st.header("TPOT")
+
+    # filename_2 = "dat/" + shap_dict[model2_select] + ".bz2"
+    filename_2 = "dat/tpot_explainer.bz2"
+
+    explainer_2 = joblib.load(filename=filename_2)
+    shap.initjs()
+    shap.force_plot(explainer_2.expected_value, tpot_shap_local[0], \
+                tree_test_df.iloc[5,:])
     
